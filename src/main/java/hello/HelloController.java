0a06f2d6d80a905e3ac5d9af47120dc92605b2a6
9148123118 <114432978+9148123118@users.noreply.github.com>
2022-11-09 21:41:54 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello, welcome to Job less chai...  !!!";
+        return "Hello,..  !!!";
     }
     
 }
